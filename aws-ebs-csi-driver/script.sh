6c00e369095bc071338d7e857eef07e4fd447807
Pham Quang Duong <phamquangduong2002@gmail.com>
2024-06-05 11:20:26 +0700

    first commit

---
+++ aws-ebs-csi-driver/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+aws iam create-role \
+  --role-name AmazonEKS_EBS_CSI_DriverRole \
+  --assume-role-policy-document file://"aws-ebs-csi-driver-trust-policy.json"
+
+aws iam attach-role-policy \
+  --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy \
+  --role-name AmazonEKS_EBS_CSI_DriverRole
